415205faf436ae8bfd8af2cf03b424da09629c40
Graham Goldstein <g.goldstein@live.com>
2018-12-02 10:34:00 +0000

    Run repeatedly, with a month's worth of past data

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ sesh = scraper.Site_Session()
 
 #for historic_weeks_beginning = 
 
-for days_ago in range(1, 365*5, 7): # Once a week for the past 5 years
-	applications = sesh.applications_for_week_commencing(datetime.datetime.now() - datetime.timedelta(days=days_ago))
+while True:
+	for days_ago in range(1, 30, 7): # Once a week for the past month
+		applications = sesh.applications_for_week_commencing(datetime.datetime.now() - datetime.timedelta(days=days_ago))
 
-	for application in applications:
-		print(application)
-		post_application.post_application(application)
+		for application in applications:
+			print(application)
+			post_application.post_application(application)
+	sleep(60*60*23) # Sleep for almost a day, to make sure we do run every day.
 
